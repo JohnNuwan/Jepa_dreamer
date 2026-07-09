29e86bd49e8a86bcf62b658cd518436b857e4b0f
Aza Money <aza@money.com>
2026-07-09 11:02:23 +0200

    Nettoyage: anciens fichiers supprimés ou déplacés dans legacy/

--- run_fast.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/aza/ftmo_agent
-source venv/bin/activate
-exec python3 -u train_fast.py 3000
--- start_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/aza/ftmo_agent
-source venv/bin/activate
-exec python3 -u server.py checkpoints/best_model.pt 9999
